bdf4242add010832700b9adcc435b03a066538db
J-CamiloG <jc6381878@gmail.com>
2024-03-28 14:36:56 -0500

    template portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+const typed = new Typed('#element', {
+    strings: ['<i class="banner_typed">Web Developer.</i> ', '&amp; a UIX.'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 500,
+    startDelay: 1000,
+
+    cursorChar: '|',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+  });
